3a77dc7d8533f76996d52f1e9c344b624b90c02b
pok12345 <shevelevpk@gmail.com>
2023-08-10 23:06:29 +0300

    add MBPlayerMove, fixed tribal_gladiator prefab, scene_test_bruh changed

--- Assets/code/player/MBPlayerMove.cs
+++ Assets/code/player/MBPlayerMove.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
 
 public class MBPlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]
+    private int moveSpeed;
+    
+    private Rigidbody2D rigidbody2d;
+    
+    public void Awake() {
+        this.rigidbody2d = this.GetComponent<Rigidbody2D>();
+        //playerInput.onActionTriggered += PlayerInput_onActionTriggered; 
+        /*Tribal_gladiator tribalGladiator  = new Tribal_gladiator();
+        tribalGladiator.Player.Enable();
+        tribalGladiator.Player.Move.performed += Move_performed;*/
+    }
+    
+    public void Move_performed(InputAction.CallbackContext context) {
+        Debug.Log(context);
+        Vector2 inputVector = context.ReadValue<Vector2>();
+        rigidbody2d.velocity = new Vector2(inputVector.x, inputVector.y) * moveSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
